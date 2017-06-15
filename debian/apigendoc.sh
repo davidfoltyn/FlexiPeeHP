646af8b3869d62e18aa02f7be79bcb13d38daecb
David Foltyn <david.foltyn@peckadesign.cz>
2017-06-15 22:25:26 +0200

    update

--- debian/apigendoc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/apigen generate --source src --destination docs --title "FlexiPeeHP `cat debian/version` " --charset UTF-8 --access-levels public --access-levels protected --php --tree
--- debian/fixversion.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VERSION=`cat debian/composer.json | grep version | awk -F'"' '{print $4}'`
-REVISION=`cat debian/revision | perl -ne 'chomp; print join(".", splice(@{[split/\./,$_]}, 0, -1), map {++$_} pop @{[split/\./,$_]}), "\n";'`
-echo ${VERSION}.${REVISION}
-sed -i -e '/\"version\"/c\    \"version\": \"'${VERSION}'.'${REVISION}'",' debian/flexipeehp/usr/share/php/FlexiPeeHP/composer.json
-
-sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSION}.${REVISION}';" debian/flexipeehp/usr/share/php/FlexiPeeHP/FlexiBeeRO.php
-sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSION}.${REVISION}';" src/FlexiPeeHP/FlexiBeeRO.php
